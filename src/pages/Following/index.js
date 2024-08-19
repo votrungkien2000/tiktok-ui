9c25e128fe5b123fcff5244ea48ab3ce89adcf3e
votrungkien2000 <kiensua2000@gmail.com>
2024-08-19 15:48:34 +0700

    base ui

---
+++ src/pages/Following/index.js
@@ -0,0 +1,7 @@
+function Following() {
+    return (
+        <h2>Following pages</h2>
+    );
+}
+
+export default Following;
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following }
+]
+const privateRoutes = []
+
+export { publicRoutes, privateRoutes }
