d39fbd4c5034b700fab989fecd838c2f3f851e38
votrungkien2000 <kiensua2000@gmail.com>
2024-09-17 19:52:20 +0700

    create-base-header

--- src/component/Layout/component/Header/index.js
+++ src/component/Layout/component/Header/index.js
@@ -1,5 +1,8 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
+import images from '@/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
 
@@ -9,8 +12,23 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/*logo*/}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok logo" />
+                </div>
                 {/*search*/}
-
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck='false' />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button >
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('sreach-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    action
+                </div>
             </div>
         </header>
     );
