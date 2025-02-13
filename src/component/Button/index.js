f8af4dace35b3b9922a0675429eec2971b8c2f4e
LAPTOP-3P4KA4BV\ACER - PC <kiensua2000@gmail.com>
2025-02-13 16:40:33 +0700

    create button

---
+++ src/component/Button/index.js
@@ -0,0 +1,9 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+
+function Button() {
+  const cx = classNames.bind(styles);
+
+  return <div></div>;
+}
+export default Button;
